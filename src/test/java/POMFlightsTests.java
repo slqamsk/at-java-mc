5815b262abb3d252a1123f45219038187dedb8e0
Sergey Slesarev <slqamsk@gmail.com>
2026-01-13 12:42:12 +0300

    Modify POMFlightsTests

--- src/test/java/POMFlightsTests.java
+++ src/test/java/POMFlightsTests.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-@Execution(ExecutionMode.CONCURRENT)
+//@Execution(ExecutionMode.CONCURRENT)
 @TestMethodOrder(MethodOrderer.DisplayName.class)
 public class POMFlightsTests {
     @BeforeAll
